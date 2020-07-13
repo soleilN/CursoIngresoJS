f8d5cbb962a9579543ead1ce09832fa2cc633235
soleilN <60760387+soleilN@users.noreply.github.com>
2020-07-13 12:22:50 -0300

    clase 1

---
+++ jsEntradaSalida-04.js
@@ -0,0 +1,13 @@
+/* 
+Debemos lograr tomar un dato por 'PROMPT' 
+y lo muestro por 'ID' al presionar el botón 'mostrar'
+*/
+function mostrar()
+{
+	var nombre;
+	nombre = prompt("Ingresa tu nombre", "Natalia Natalia");
+	txtIdNombre.value=nombre;
+	//document.getElementById('txtIdNombre').value=nombre;
+}
+	
+
---
+++ jsEntradaSalida-05.js
@@ -0,0 +1,13 @@
+/*
+Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function mostrar()
+{	
+	var edadIngresada;
+	var nombreIgresado;
+	nombreIgresado=txtIdNombre.value;
+	edadIngresada=txtIdEdad.value;
+	alert("usted se llama "+ nombreIgresado+" y tiene "+edadIngresada+" años");
+
+}
+
